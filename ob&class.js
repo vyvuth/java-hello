d7bf85e190cb77ed06cfced0903f32de5bb21a6d
vyvuth <vyvuth191@gmail.com>
2024-06-16 13:04:03 +0700

    add files 1:04

--- ob&class.js
+++ ob&class.js
@@ -18,6 +18,38 @@
         }
     }
     const result = new test("500$","250$");
-    result.addItem("apple");
-    result.addItem("Orange");
-    console.log(result);
+   // result.addItem("apple");
+   // result.addItem("Orange");
+   // console.log(result);
+    let  ArrayObject = [
+        {id:1001,Name:"VY", Age:15, Sex:"F"},
+        {id:1002,Name:"VY", Age:21, Sex:"F"},
+        {id:1003,Name:"VY", Age:21, Sex:"M"},
+        {id:1004,Name:"VY", Age:21, Sex:"M"},
+        {id:1005,Name:"VY", Age:21, Sex:"M"},
+    ];
+   // ArrayObject.pop(); // remove last element
+   // ArrayObject.shift();
+   //  console.log(ArrayObject.map ((e)=>e));
+   //console.log(ArrayObject.filter((e)=> e.Age < 18 && e.Sex==="F"));
+
+   var myNumber= [10,20,30];
+   //console.log(myNumber.reduce((p,c)=>p+c));
+
+   // method sort
+   let number = [10, 20, 30, 40];
+   let month = ["mar", "feb", "apri"];
+   
+   function compar(a, b) {
+       if (a > b) {
+           return 1;
+       } else if (a < b) {
+           return -1;
+       } else {
+           return 0;
+       }
+   }
+   console.log(month.sort((a,b)=>a-b))
+   //console.log(month.sort());
+   
+   
