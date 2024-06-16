7658994674ad49a3498d5ff9ca0aee47d32ec494
vyvuth <vyvuth191@gmail.com>
2024-06-16 11:57:27 +0700

    add files 16

--- ob&class.js
+++ ob&class.js
@@ -8,10 +8,16 @@
     class test{
         price;
         total;
+        item=[];
         constructor(price,total){
             this.price = price;
             this.total = total;
         }
+        addItem(text){
+            this.item.push(text);
+        }
     }
     const result = new test("500$","250$");
+    result.addItem("apple");
+    result.addItem("Orange");
     console.log(result);
